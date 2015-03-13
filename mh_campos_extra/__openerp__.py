1cb64cf3bf9a8e9cc5d990c505f360d04ecb7928
Said Kuri <skuri@tantums.com>
2015-03-13 13:19:42 -0600

    [FIX] cambio de campos product.product to template

--- mh_campos_extra/__openerp__.py
+++ mh_campos_extra/__openerp__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        'templates.xml','view/product_product_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
