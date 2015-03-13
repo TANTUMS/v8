e7bbf6c479d88af8d2de7dccd09d5c2739112b25
Said Kuri <skuri@tantums.com>
2015-03-13 16:11:46 -0600

    [ADD] Campo precio empleado

--- mh_campos_extra/models.py
+++ mh_campos_extra/models.py
@@ -2,31 +2,15 @@
 
 from openerp import models, fields, api
 
-# class mh_campos_extra(models.Model):
-# 	_inherit = 'product.product'
-
-# 	id_type = fields.Integer(string='Id Type')
-# 	code   = fields.Integer(string='Code')
-# 	id_um   = fields.Integer(string='Id UM')
-# 	barcode = fields.Char('BarCode')
-# 	id_iva_type = fields.Integer('Id IVA Type')
-# 	#mule_sync = fields.Boolean(string='Mule Sync Flag',default=False,index=True)
-# 	mule_sync = fields.Boolean(compute='_actualiza_cambio',default=False,index=True,store=True,string='Mule Sync Flag')
-
-# 	@api.depends('default_code','active','product_tmpl_id.list_price','product_tmpl_id.name')
-# 	def _actualiza_cambio(self):
-# 		for rec in self:
-# 			self.mule_sync = False
-
-
 class mh_campos_extra(models.Model):
 	_inherit = 'product.template'
 
 	code   = fields.Integer(string='Code')
 	barcode = fields.Char('BarCode')
+	employee_price = fields.Float('Employee Price')
 	mule_sync = fields.Boolean(compute='_actualiza_cambio',default=False,index=True,store=True,string='Mule Sync Flag')
 
-	@api.depends('active','list_price','name')
+	@api.depends('active','list_price','name','employee_price','barcode','code')
 	def _actualiza_cambio(self):
 		for rec in self:
 			self.mule_sync = False
