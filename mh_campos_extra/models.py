31c24d3f534bb45fe72a36897842081bf20fcd91
Said Kuri <skuri@tantums.com>
2015-03-12 21:36:21 -0600

    [ADD] Computed field para notificar cambios de producto

--- mh_campos_extra/models.py
+++ mh_campos_extra/models.py
@@ -5,15 +5,15 @@ from openerp import models, fields, api
 class mh_campos_extra(models.Model):
 	_inherit = 'product.product'
 
-
 	id_type = fields.Integer(string='Id Type')
 	code   = fields.Integer(string='Code')
 	id_um   = fields.Integer(string='Id UM')
 	barcode = fields.Char('BarCode')
 	id_iva_type = fields.Integer('Id IVA Type')
-	mule_sync = fields.Boolean(string='Mule Sync Flag',default=False,index=True)
+	#mule_sync = fields.Boolean(string='Mule Sync Flag',default=False,index=True)
+	mule_sync = fields.Boolean(compute='_actualiza_cambio',default=False,index=True,store=True,string='Mule Sync Flag')
 
-
-#     _name = 'mh_campos_extra.mh_campos_extra'
-
-#     name = fields.Char()
+	@api.depends('default_code','active','product_tmpl_id.list_price','product_tmpl_id.name')
+	def _actualiza_cambio(self):
+		for rec in self:
+			self.mule_sync = False
