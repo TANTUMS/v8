ae08fa2c4143fdd7dad2bd6791131797718621db
Said Kuri <skuri@tantums.com>
2015-03-19 12:03:50 -0600

    [ADD] Campo Bom Line Sync

--- mh_campos_extra/models.py
+++ mh_campos_extra/models.py
@@ -11,7 +11,17 @@ class mh_campos_extra(models.Model):
 	promotion = fields.Boolean('Promotion',default=False)
 	mule_sync = fields.Boolean(compute='_actualiza_cambio',default=False,index=True,store=True,string='Mule Sync Flag')
 
-	@api.depends('active','list_price','name','employee_price','barcode','code')
+	@api.depends('active','list_price','name','employee_price','barcode','code','promotion')
 	def _actualiza_cambio(self):
 		for rec in self:
 			self.mule_sync = False
+
+class mh_campos_extra_mrp(models.Model):
+	_inherit = 'mrp.bom.line'
+
+	bom_mule_sync = fields.Boolean(compute='_actualiza_cambio_mrp',default=False,index=True,store=True,string='Mule Sync Flag')
+
+	@api.depends('product_id','product_qty','product_uom')
+	def _actualiza_cambio_mrp(self):
+		for rec in self:
+			self.bom_mule_sync = False
